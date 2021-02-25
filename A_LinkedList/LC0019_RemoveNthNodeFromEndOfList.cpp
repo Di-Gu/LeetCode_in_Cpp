28c0d84bf5318520865e4a510eca0d0c52ba6b09
gudi <isgudi00@gmail.com>
2021-02-25 14:25:54 +0800

    LC19 remove nth number from end of list

---
+++ A_LinkedList/LC0019_RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,33 @@
+//
+// Created by isgud on 2/20/2021.
+//
+
+#include "LinkListSolutionClass.h"
+
+ListNode* LC0019_RemoveNthNodeFromEndOfList::removeNthFromEnd(ListNode* head, int n) {
+    if(!head) return nullptr;
+
+    auto *dummy = new ListNode(0);
+    dummy->next = head;
+
+    ListNode *fast = dummy;
+    ListNode *slow = dummy;
+
+    while(fast && n >= 0) {
+        fast = fast->next;
+        n--;
+    }
+
+    while(fast) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    // nice trick !!
+    /*for (;fast->next;fast = fast->next)
+        slow = slow->next;*/
+
+    slow->next = slow->next->next;
+
+    return dummy->next;
+};
