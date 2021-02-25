d7e8c09c46b7a806d4b99c28919c83d5dd83e5fd
gudi <isgudi00@gmail.com>
2021-02-25 14:26:15 +0800

    LinkedList Solution Class

---
+++ A_LinkedList/LinkListSolutionClass.h
@@ -0,0 +1,17 @@
+/**********************************************************************************************************************
+    > File Name: LinkListSolutionClass.h
+    > Author: isgud 
+    > Date: 2/25/2021
+**********************************************************************************************************************/
+
+#ifndef LEETCODE_IN_CPP_LINKLISTSOLUTIONCLASS_H
+#define LEETCODE_IN_CPP_LINKLISTSOLUTIONCLASS_H
+
+#include "data_struct.h"
+
+class LC0019_RemoveNthNodeFromEndOfList {
+public:
+    static ListNode* removeNthFromEnd(ListNode* head, int n);
+};
+
+#endif //LEETCODE_IN_CPP_LINKLISTSOLUTIONCLASS_H
